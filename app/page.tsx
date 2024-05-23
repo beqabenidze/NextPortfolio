d906497b531d425808828b394f2815064d583a2d
Beqa Benidze <beqabenidze9@gmail.com>
2024-05-23 21:38:45 +0400

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import HERO from "@/components/HERO";
+import Hero from "@/components/HERO";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <HERO />
+        <Hero />
         heyy
       </div>
     </main>
