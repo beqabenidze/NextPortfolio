744450d80e49aaec6b877e6cd6d10dc76a9099c6
Beqa Benidze <beqabenidze9@gmail.com>
2024-05-27 21:31:40 +0400

    Backbone

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ const HERO = () => {
           fill="blue"
         />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[21.2]  flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
 
@@ -36,7 +36,7 @@ const HERO = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Beka, a Next.js Developer based in Georgia.
           </p>
 
           <a href="#about">
